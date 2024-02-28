79b6197098c7d0ba5b77f280df2b525c62faca7f
soham sarkar <sohamsarkar59@gmail.com>
2024-02-29 00:40:55 +0530

    made folder and start icons

---
+++ src/assests/start.svelte
@@ -0,0 +1,64 @@
+<svg
+   width="48"
+   height="48"
+   viewBox="0 0 36 36"
+   version="1.1"
+   id="svg5"
+   xmlns="http://www.w3.org/2000/svg">
+  <defs id="defs2">
+    <linearGradient
+       id="a"
+       x1="-197.72"
+       x2="-197.72"
+       y1="263.86"
+       y2="273.76"
+       gradientTransform="rotate(-45 -337.55 -145.8)"
+       gradientUnits="userSpaceOnUse">
+      <stop offset="0" id="stop826" />
+      <stop stop-opacity="0" offset="1" id="stop828" />
+    </linearGradient>
+  </defs>
+  <g id="layer1">
+    <g
+       id="g14"
+       transform="matrix(0.85572537,0,0,0.85572537,-2.5374087,-2.5374087)"
+       style="fill:#ffffff;fill-opacity:1">
+      <rect
+         style="fill:#ffffff;fill-rule:evenodd;stroke:none;stroke-width:0.312438;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1;fill-opacity:1"
+         id="rect2854"
+         width="14.997024"
+         height="14.997024"
+         x="25.363365"
+         y="7.6396108"
+         rx="2.9538949"
+         ry="2.9538949" />
+      <rect
+         style="fill:#ffffff;fill-rule:evenodd;stroke:none;stroke-width:0.312438;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1;fill-opacity:1"
+         id="rect2952"
+         width="14.997024"
+         height="14.997024"
+         x="25.363365"
+         y="25.363365"
+         rx="2.9538949"
+         ry="2.9538949" />
+      <rect
+         style="fill:#ffffff;fill-rule:evenodd;stroke:none;stroke-width:0.312438;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1;fill-opacity:1"
+         id="rect3034"
+         width="14.997024"
+         height="14.997024"
+         x="7.6396108"
+         y="7.6396108"
+         rx="2.9538949"
+         ry="2.9538949" />
+      <rect
+         style="fill:#ffffff;fill-rule:evenodd;stroke:none;stroke-width:0.312438;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1;fill-opacity:1"
+         id="rect3116"
+         width="14.997024"
+         height="14.997024"
+         x="7.6396108"
+         y="25.363365"
+         rx="2.9538949"
+         ry="2.9538949" />
+    </g>
+  </g>
+</svg>
