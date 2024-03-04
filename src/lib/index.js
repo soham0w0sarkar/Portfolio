5c7c73946c460f7ac92b9252205be23a1184f3f8
soham sarkar <sohamsarkar59@gmail.com>
2024-03-04 15:29:14 +0530

    added imports for Terminal and Window components for $lib alias

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,1 +1,5 @@
 // place files you want to import through the `$lib` alias in this folder.
+import Terminal from "../components/terminal.svelte"
+import Window from "../components/window.svelte"
+
+export { Terminal, Window }
