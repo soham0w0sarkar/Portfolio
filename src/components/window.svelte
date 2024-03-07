deb104834d18b0cd76c95d9d10c51459c683b12b
soham sarkar <sohamsarkar59@gmail.com>
2024-03-08 02:09:48 +0530

    updated window title to "Terminal"

--- src/components/window.svelte
+++ src/components/window.svelte
@@ -13,7 +13,7 @@
 >
 	<div class="w-full h-fit variant-filled-surface p-2">
 		<div class="flex justify-between items-center h-full w-full">
-			<div class="w-full h-full text-center font-extrabold">Title</div>
+			<div class="w-full h-full text-center font-extrabold">Terminal</div>
 			<span class="h-full w-fit flex p-1">
 				<button
 					class="w-4 h-4 bg-yellow-500 rounded-full mr-2 flex justify-center items-center shadow-md"
