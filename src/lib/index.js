ee605b09a14c8ca60d3cc977b6af191f1a2f1bce
soham sarkar <sohamsarkar59@gmail.com>
2024-03-08 23:48:49 +0530

    refactored terminal component for prearing for different types of terminal

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,5 +1,6 @@
 // place files you want to import through the `$lib` alias in this folder.
-import Terminal from "../components/terminal.svelte"
-import Window from "../components/window.svelte"
+import Terminal from '../components/terminal.svelte';
+import Window from '../components/window.svelte';
+import Material from '../components/terminals/material.svelte';
 
-export { Terminal, Window }
+export { Terminal, Window, Material };
