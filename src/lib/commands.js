fc259337c8e6d4bbe9f96ef88e06e98f3c06c954
soham sarkar <sohamsarkar59@gmail.com>
2024-03-24 21:28:38 +0530

    continuous refactoring

--- src/lib/commands.js
+++ src/lib/commands.js
@@ -1,78 +1,128 @@
+import { commandHistory, commandLine } from '$lib/store';
 /**
  * @param {string} command
  * @returns {void}
  */
 export default function handleCommand(command) {
-    switch (command) {
-        case 'clear':
-            clear();
-            break;
-        case 'portfolio':
-            portfolio();
-            break;
-        case 'about':
-            about();
-            break;
-        case 'contact':
-            contact();
-            break;
-        case 'skills':
-            skills();
-            break;
-        case 'projects':
-            projects();
-            break;
-        case 'education':
-            education();
-            break;
-        case 'experience':
-            experience();
-            break;
-        case 'resume':
-            resume();
-            break;
-        case 'exit':
-            exit();
-            break;
-    }
+	switch (command) {
+		case 'clear':
+			clear();
+			break;
+		case 'portfolio':
+			portfolio();
+			break;
+		case 'about':
+			about();
+			break;
+		case 'contact':
+			contact();
+			break;
+		case 'skills':
+			skills();
+			break;
+		case 'projects':
+			projects();
+			break;
+		case 'education':
+			education();
+			break;
+		case 'experience':
+			experience();
+			break;
+		case 'resume':
+			resume();
+			break;
+		case 'exit':
+			exit();
+			break;
+	}
+	addCommandToHistory(command);
+}
+
+/**
+ * @param {string} output
+ * @param {string} status
+ * @param {string} command
+ * @returns {void}
+ */
+
+const updateCommandLine = (command, output, status) => {
+	commandLine.update((commandLine) => {
+		commandLine[commandLine.length - 1] = {
+			command,
+			output,
+			status
+		};
+		return commandLine;
+	});
+
+	const newCommandLine = {
+		command: '',
+		output: '',
+		status: 'current'
+	};
+    
+	commandLine.update((commandLine) => {
+		commandLine.push(newCommandLine);
+		return commandLine;
+	});
+};
+
+/**
+ * @param {string} command
+ * @returns {void}
+ */
+
+const addCommandToHistory = (command) => {
+	commandHistory.update((commandHistory) => {
+		commandHistory.push(command);
+		return commandHistory;
+	});
 };
 
 const clear = () => {
-    console.log('clear');
+	const newCommandLine = [
+		{
+			command: '',
+			output: '',
+			status: 'current'
+		}
+	];
+	commandLine.set(newCommandLine);
 };
 
 const portfolio = () => {
-    console.log('portfolio');
+	console.log('portfolio');
 };
 
 const about = () => {
-    console.log('about');
+	console.log('about');
 };
 
 const contact = () => {
-    console.log('contact');
+	console.log('contact');
 };
 
 const skills = () => {
-    console.log('skills');
+	console.log('skills');
 };
 
 const projects = () => {
-    console.log('projects');
+	console.log('projects');
 };
 
 const education = () => {
-    console.log('education');
+	console.log('education');
 };
 
 const experience = () => {
-    console.log('experience');
+	console.log('experience');
 };
 
 const resume = () => {
-    console.log('resume');
+	console.log('resume');
 };
 
 const exit = () => {
-    console.log('exit');
+	console.log('exit');
 };
--- src/lib/store.js
+++ src/lib/store.js
@@ -1,6 +1,6 @@
 import  { writable }  from "svelte/store";
 
-export const commandHistory = writable([]);
+export const commandHistory = writable(['']);
 export const commandLine = writable([
     {
         command: '',
