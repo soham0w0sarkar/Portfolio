8ed9ad82fc89e969b68983c2ed3e630ee419faef
Soham Sarkar <sohamsarkar59@gmail.com>
2024-04-18 12:38:39 +0530

    structural changes

--- src/components/outputs/skills.svelte
+++ src/components/outputs/skills.svelte
@@ -1,4 +1,8 @@
 <div class="outline w-full h-96 p-2">
-	<h1>skills</h1>
-	<h2>Frontend:</h2>
+	<h1>Contact:</h1>
+	<h2>Name:</h2>
+	<input
+		type="text"
+		class="inputs bg-inherit p-0 w-full border-0 text-2xl border-transparent focus:border-transparent focus:ring-0"
+	/>
 </div>
