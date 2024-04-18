82fe781bef38a9e31d423910813b58a832838e0f
Soham Sarkar <sohamsarkar59@gmail.com>
2024-04-18 12:39:23 +0530

    adding new component

--- src/lib/index.js
+++ src/lib/index.js
@@ -4,5 +4,6 @@ import Window from '../components/window.svelte';
 import Material from '../components/terminals/material.svelte';
 import AtomicBit from '../components/terminals/atomicBit.svelte';
 import Skills from '../components/outputs/skills.svelte';
+import Contact from '../components/outputs/contact.svelte';
 
-export { Terminal, Window, Material, AtomicBit, Skills };
+export { Terminal, Window, Material, AtomicBit, Skills, Contact };
