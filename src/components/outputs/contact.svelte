3a4e23e1eb5954cd48de67af001e50a2a3b9bc5d
Soham Sarkar <sohamsarkar59@gmail.com>
2024-04-18 12:37:52 +0530

    making components for each command

---
+++ src/components/outputs/contact.svelte
@@ -0,0 +1,26 @@
+<div class="outline w-full h-96 p-2">
+	<h1>Contact:</h1>
+	<span class="flex">
+		<h2>Name:</h2>
+		&nbsp;
+		<input
+			type="text"
+			class="inputs bg-inherit p-0 w-full border-0 text-2xl border-transparent focus:border-transparent focus:ring-0"
+		/>
+	</span>
+	<span class="flex">
+		<h2>Email:</h2>
+		&nbsp;
+		<input
+			type="email"
+			class="inputs bg-inherit p-0 w-full border-0 text-2xl border-transparent focus:border-transparent focus:ring-0"
+		/>
+	</span>
+	<span class="flex flex-col">
+		<h2>Message:</h2>
+		<input
+			type="text"
+			class="inputs bg-inherit p-0 w-full border-0 text-2xl border-transparent focus:border-transparent focus:ring-0"
+		/>
+	</span>
+</div>
