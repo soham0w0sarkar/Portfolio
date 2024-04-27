83973302ede24961bf2fb715d186108bbab7f529
Soham Sarkar <sohamsarkar59@gmail.com>
2024-04-27 23:25:14 +0530

    changed style of the loader

--- src/components/loader.svelte
+++ src/components/loader.svelte
@@ -5,9 +5,6 @@
 	<div></div>
 	<div></div>
 	<div></div>
-	<div></div>
-	<div></div>
-	<div></div>
 </div>
 
 <style>
@@ -18,69 +15,56 @@
 	.lds-grid {
 		display: inline-block;
 		position: relative;
-		width: 40px;
-		height: 40px;
+		width: 12px;
+		height: 24px;
 	}
 	.lds-grid div {
 		position: absolute;
-		width: 5px;
-		height: 5px;
+		width: 8px;
+		height: 8px;
 		border-radius: 50%;
 		background: currentColor;
 		animation: lds-grid 1.2s linear infinite;
 	}
 	.lds-grid div:nth-child(1) {
-		top: 8px;
-		left: 8px;
-		animation-delay: 0s;
+		top: 0;
+		left: 0;
+		animation-delay: 0s; /* Adjusted delay for first circle */
 	}
 	.lds-grid div:nth-child(2) {
-		top: 8px;
-		left: 32px;
-		animation-delay: -0.4s;
+		top: 0;
+		left: 12px;
+		animation-delay: 0.2s; /* Adjusted delay for second circle */
 	}
 	.lds-grid div:nth-child(3) {
-		top: 8px;
-		left: 56px;
-		animation-delay: -0.8s;
+		top: 12px;
+		left: 12px;
+		animation-delay: 0.4s; /* Adjusted delay for third circle */
 	}
 	.lds-grid div:nth-child(4) {
-		top: 32px;
-		left: 8px;
-		animation-delay: -0.4s;
+		top: 24px;
+		left: 12px;
+		animation-delay: 0.6s; /* Adjusted delay for fourth circle */
 	}
 	.lds-grid div:nth-child(5) {
-		top: 32px;
-		left: 32px;
-		animation-delay: -0.8s;
+		top: 24px;
+		left: 0;
+		animation-delay: 0.8s; /* Adjusted delay for fifth circle */
 	}
 	.lds-grid div:nth-child(6) {
-		top: 32px;
-		left: 56px;
-		animation-delay: -1.2s;
-	}
-	.lds-grid div:nth-child(7) {
-		top: 56px;
-		left: 8px;
-		animation-delay: -0.8s;
-	}
-	.lds-grid div:nth-child(8) {
-		top: 56px;
-		left: 32px;
-		animation-delay: -1.2s;
-	}
-	.lds-grid div:nth-child(9) {
-		top: 56px;
-		left: 56px;
-		animation-delay: -1.6s;
+		top: 12px;
+		left: 0;
+		animation-delay: 1s; /* Adjusted delay for sixth circle */
 	}
 	@keyframes lds-grid {
 		0%,
 		100% {
 			opacity: 1;
+			transform: scale(1);
 		}
 		50% {
 			opacity: 0.5;
+			transform: scale(0.8);
 		}
 	}
 </style>
